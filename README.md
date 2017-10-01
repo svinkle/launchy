9b3cb7bd3a126223bbb1791f1e1edeb9e6ac8a8c
Scott Vinkle <svinkle@gmail.com>
2017-10-01 15:32:38 -0400

    Added instanbul in order to setup codecov.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Build Status](https://travis-ci.org/svinkle/launchy.svg?branch=master)](https://travis-ci.org/svinkle/launchy)
+[![codecov](https://codecov.io/gh/svinkle/launchy/branch/master/graph/badge.svg)](https://codecov.io/gh/svinkle/launchy)
 
 # Launchy! 🚀
 
--- test/index.js
+++ test/index.js
@@ -10,18 +10,17 @@ describe('Launchy', () => {
     });
 
     describe('Elements…', () => {
+        it('with `data-launchy` to exist', (done) => {
+            expect(fixture.el.querySelector('[data-launchy]')).toBeTruthy();
+            done();
+        });
 
-        // it('with `data-launchy` to exist', (done) => {
-        //     expect(fixture.el.querySelector('[data-launchy]')).toBeTruthy();
-        //     done();
-        // });
-        //
-        // it('launcher control to exist', (done) => {
-        //     setTimeout(() => {
-        //         expect(fixture.el.querySelector('#launchy-control-0')).toBeTruthy();
-        //     }, 500);
-        //     done();
-        // });
+        it('launcher control to exist', (done) => {
+            setTimeout(() => {
+                expect(fixture.el.querySelector('#launchy-control-0')).toBeTruthy();
+            }, 500);
+            done();
+        });
 
         // TODO: write more dang tests!
     });
